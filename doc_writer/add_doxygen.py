e3fa6a9f2288bb748e867f5ed15b7c032c488f5c
CharlesDu <dohoney@126.com>
2023-12-04 14:49:00 -0800

    add document in multiple chat turns

--- doc_writer/add_doxygen.py
+++ doc_writer/add_doxygen.py
@@ -126,37 +126,122 @@ def add_doxygen(code_content):
     """
     Sends code content to GPT API to add Doxygen documentation.
     """
-    # This code is for v1 of the openai package: pypi.org/project/openai
     client = OpenAI()
 
     response = client.chat.completions.create(
-    model="gpt-4-1106-preview",
-    # model='gpt-3.5-turbo-1106',
-    messages=[
-        {
-        "role": "system",
-        "content": "You are a C++ programmer and good at writing doxygen documentation for C++ headers. \
-        The user will provide the content of a C++ header file, and you will return the code with doxygen documentation added. \
-        You should only include the documented code in your response, without any explanation. \
-        If you cannot finish the task in one response, you should return the code with doxygen documentation added so far \
-        and add '//to be continued' as the last line of your response."
-        },
-        {
-        "role": "user",
-        "content": f"Add Doxygen documentation to the following C++ code: \n{code_content}"
-        }
-    ],
-    temperature=1,
-    max_tokens=4095,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+        model="gpt-4-1106-preview",
+        # model='gpt-3.5-turbo-1106',
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a C++ programmer and good at writing doxygen documentation for C++ headers. \
+                The user will provide the content of a C++ header file, and you will return the code with doxygen documentation added. \
+                You should only include the documented code in your response, without any explanation. \
+                If you cannot finish the task in one response, you should return the code with doxygen documentation added so far \
+                and add '//to be continued' as the last line of your response."
+            },
+            {
+                "role": "user",
+                "content": f"Add Doxygen documentation to the following C++ code: \n{code_content}"
+            }
+        ],
+        temperature=1,
+        max_tokens=4095,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
 
-    if response.choices[0].finish_reason == 'stop':
-        output = response.choices[0].message.content
-        print(output)
-        return output
+    # if finish_reason is not 'stop', report error and exit
+    if response.choices[0].finish_reason != 'stop':
+        print("Error: GPT failed to finish the task.")
+        return
+
+    output = response.choices[0].message.content
+    # Remove '```cpp' and '```' from the output
+    output = output.replace('```cpp', '')
+    output = output.replace('```', '')
+
+    return output
+
+def add_doxygen_multi(code_content):
+    """
+    Sends code content to GPT API to add Doxygen documentation.
+    """
+    client = OpenAI()
+
+    chat_history = [
+            {
+                "role": "system",
+                "content": "You are a C++ programmer and good at writing doxygen documentation for C++ headers. \
+                The user will provide the content of a C++ header file, and you will return the code with doxygen documentation added. \
+                You should only include the documented code in your response, without any explanation. \
+                If you cannot finish the task in one response, you should return the code with doxygen documentation added so far \
+                and add '//continue' as the last line of your response."
+            },
+            {
+                "role": "user",
+                "content": f"Add Doxygen documentation to the following C++ code: \n{code_content}"
+            }
+        ]
+
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        # model='gpt-3.5-turbo-1106',
+        messages=chat_history,
+        temperature=1,
+        max_tokens=4095,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # if finish_reason is not 'stop', report error and exit
+    if response.choices[0].finish_reason != 'stop':
+        print("Error: GPT failed to finish the task.")
+        return
+    response_list = [response.choices[0].message.content]
+    chat_history.append(response.choices[0].message)
+
+    # While '//continue' is in the current response, continue to send requests
+    iter = 0
+    max_iter = 5
+    while '//continue' in response_list[-1] and iter < max_iter:
+        print("iter: ", iter)
+        iter += 1
+        chat_history.append({
+            "role": "user",
+            "content": "Please continue adding doxygen documentation from the previous response. \
+            Please only include the documented code in your response, without any explanation. \
+            If you cannot finish the task in one response, you should return the code with doxygen documentation added so far \
+            and add '//continue' as the last line of your response."
+        })
+        response = client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            # model='gpt-3.5-turbo-1106',
+            messages=chat_history,
+            temperature=1,
+            max_tokens=4095,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        if response.choices[0].finish_reason != 'stop':
+            print("Error: GPT failed to finish the task.")
+            return
+        response_list.append(response.choices[0].message.content)
+        chat_history.append(response.choices[0].message)
+
+    # Merge all responses into one
+    output = '\n'.join(response_list)
+    # Remove '```cpp' and '```' from the output
+    output = output.replace('```cpp', '')
+    output = output.replace('```', '')
+    # Remove '//to be continued' from the output
+    output = output.replace('//continue', '')
+
+    return output
+
 
 # def process_cpp_file(input_file_path, output_file_path, max_chunk_length):
 #     """
@@ -181,7 +266,7 @@ def process_cpp_file(input_file_path, output_file_path, max_chunk_length):
         cpp_code = file.read()
 
     cpp_code = clean_code_content(cpp_code)
-    doc_code = add_doxygen(cpp_code)
+    doc_code = add_doxygen_multi(cpp_code)
     if doc_code:
         with open(output_file_path, 'w') as file:
             file.write(doc_code)
