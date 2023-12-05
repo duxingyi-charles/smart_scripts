2ca18fc4a0a5452b919cf6c00c88c00087d80efa
xingyi <xingyidu@global.tencent.com>
2023-12-04 16:14:04 -0800

    clear outdated code

--- doc_writer/add_doxygen.py
+++ doc_writer/add_doxygen.py
@@ -29,61 +29,6 @@ def clean_code_content(code_content):
 
     return code_content
 
-# def clean_code_content(code_content):
-#     """
-#     Cleans the C++ code content by removing unnecessary spaces and consecutive newlines,
-#     while preserving indentation.
-
-#     :param code_content: String containing the entire code content.
-#     :return: Cleaned code content.
-#     """
-#     # Remove excess white spaces within lines, preserving indentation
-#     cleaned_lines = []
-#     for line in code_content.split('\n'):
-#         # Find the first non-whitespace character
-#         first_non_space = len(line) - len(line.lstrip())
-#         # Preserve leading spaces (indentation) and remove additional spaces in the rest of the line
-#         cleaned_line = line[:first_non_space] + re.sub(r'[ \t]+', ' ', line[first_non_space:])
-#         cleaned_lines.append(cleaned_line)
-
-#     cleaned_code = '\n'.join(cleaned_lines)
-
-#     # Remove unnecessary spaces before and after braces, preserving indentation
-#     cleaned_code = re.sub(r'(\S)\s*{\s*', r'\1 { ', cleaned_code)
-#     cleaned_code = re.sub(r'\s*}\s*(\S)', r' } \1', cleaned_code)
-
-#     # Normalize newlines (remove consecutive newlines)
-#     cleaned_code = re.sub(r'\n\s*\n', '\n', cleaned_code)
-
-#     # compare length of code_content and cleaned_code
-#     print("length (before clean): ", len(code_content))
-#     print("length (after clean): ", len(cleaned_code))
-
-#     return cleaned_code
-
-
-def split_cpp_file(file_content, max_length):
-    """
-    Splits the C++ file into chunks without breaking functions.
-    """
-    chunks = []
-    current_chunk = []
-    current_length = 0
-
-    for line in file_content.split('\n'):
-        line_length = len(line) + 1  # +1 for the newline character
-        if current_length + line_length > max_length or '}' in line:
-            chunks.append('\n'.join(current_chunk))
-            current_chunk = []
-            current_length = 0
-        current_chunk.append(line)
-        current_length += line_length
-
-    if current_chunk:
-        chunks.append('\n'.join(current_chunk))
-
-    return chunks
-
 def add_doxygen_json(code_content):
     """
     Sends code content to GPT API to add Doxygen documentation.
@@ -167,6 +112,7 @@ def add_doxygen(code_content):
 def add_doxygen_multi(code_content):
     """
     Sends code content to GPT API to add Doxygen documentation.
+    Multiple requests are sent if the response is too long for one request.
     """
     client = OpenAI()
 
@@ -243,22 +189,7 @@ def add_doxygen_multi(code_content):
     return output
 
 
-# def process_cpp_file(input_file_path, output_file_path, max_chunk_length):
-#     """
-#     Process a C++ file, adding Doxygen comments to it.
-#     """
-#     with open(input_file_path, 'r') as file:
-#         cpp_code = file.read()
-
-#     cpp_code = clean_code_content(cpp_code)
-#     chunks = split_cpp_file(cpp_code, max_chunk_length)
-#     write_chunks_to_file(chunks, output_file_path)
-#     # processed_chunks = [add_doxygen_to_chunk(chunk) for chunk in chunks]
-
-#     # with open(output_file_path, 'w') as file:
-#     #     file.write('\n'.join(processed_chunks))
-
-def process_cpp_file(input_file_path, output_file_path, max_chunk_length):
+def process_cpp_file(input_file_path, output_file_path):
     """
     Process a C++ file, adding Doxygen comments to it.
     """
@@ -271,38 +202,18 @@ def process_cpp_file(input_file_path, output_file_path, max_chunk_length):
         with open(output_file_path, 'w') as file:
             file.write(doc_code)
 
-def write_chunks_to_file(chunks, output_file_path, separator='--- CHUNK SEPARATOR ---\n'):
-    """
-    Writes the chunks to a file, adding a separator between each chunk.
-    
-    :param chunks: List of string chunks.
-    :param output_file_path: Path to the output file.
-    :param separator: String to use as a separator between chunks.
-    """
-    with open(output_file_path, 'w') as file:
-        for i, chunk in enumerate(chunks):
-            if i > 0:
-                file.write(separator)
-            file.write(chunk)
-
 def main():
-    # Create the parser
-    parser = argparse.ArgumentParser(description="Generate doxygen documentation for c++ header.")
-
-    # Add the arguments
+    parser = argparse.ArgumentParser(description="Generate doxygen documentation for C++ header.")
     parser.add_argument('FilePath',
                         metavar='path',
                         type=str,
                         help='the path to the file')
-
-    # Execute the parse_args() method
     args = parser.parse_args()
 
     input_cpp_file = args.FilePath
     output_cpp_file = input_cpp_file.replace(".h", "_doxygen.h")
-    max_chunk_length = 2048  # Adjust based on GPT-4's context length limits
 
-    process_cpp_file(input_cpp_file, output_cpp_file, max_chunk_length)
+    process_cpp_file(input_cpp_file, output_cpp_file)
 
 
 if __name__ == "__main__":
